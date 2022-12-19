bbde69dfc08a3f955746b2af16e864ac7d61c9ba
Thomas1q <119288823+Thomas1q@users.noreply.github.com>
2022-12-19 09:13:40 +0100

    Add files via upload

---
+++ Schach_Klassen.py
@@ -0,0 +1,24 @@
+
+class Turm():
+    def __init__(self, color):
+        
+class Springer():
+    def __init__(self, color):
+        
+class Läufer():
+    def __init__(self, color):
+
+class Dame():
+    def __init__(self, color):
+        
+class König():
+    def __init__(self, color):
+        
+class Bauer():
+    def __init__(self, color):
+        
+
+class Move():
+    def __init__(self)
+
+        
