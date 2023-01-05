50db6fe55b96239d776630f769f38ea45c6c1174
Daniel Pfurner <dpfurner@tsn.at>
2023-01-05 19:01:59 +0100

    Reformatting

--- chess4b/logic/game.py
+++ chess4b/logic/game.py
@@ -2,6 +2,7 @@ import pygame
 
 from chess4b.logic import BaseLogic
 
+
 class GameLogic(BaseLogic):
     def __init__(self, screen: pygame.Surface = None, clock: pygame.time.Clock = None):
         super().__init__(screen, clock)
