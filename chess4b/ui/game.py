5823af19e504febaf1239ad26cb23fd06a08e5fb
Leon <lpezzei@tsn.at>
2023-01-23 09:41:27 +0100

    23.01 update

--- chess4b/ui/game.py
+++ chess4b/ui/game.py
@@ -6,6 +6,8 @@ class GameDisplay:
     def __init__(self, screen: pygame.Surface, color: chess.Color):
 
         self.color: chess.Color = color
+        self.font = pygame.font.Font('AGENCYR.ttf', 32)
+        self.text_col = 'white'
 
         self.screen: pygame.Surface = screen
         self.squares: dict={}
@@ -13,15 +15,11 @@ class GameDisplay:
         self.display_field()
         self.setup_squares()
 
-    def draw_text(text, font, text_col, x, y):
-        img = font.render(text, True, text_col)
-        screen.blit(img, (x, y))
-
     def setup_squares(self):
 
         #colors
         turquoise = (52, 78, 91)
-        #white = (255, 255, 255)
+        white = (255, 255, 255)
 
         X = 1200
         Y = 800
@@ -31,44 +29,62 @@ class GameDisplay:
 
 
 
-        color_rect = ('white')
-        color_rect_2 = (0, 0, 0)
-        font = pygame.font.Font('AGENCYR.ttf', 32)
-        TEXT_COL = ('white')
+
         # Create rects to be able to check if the mouse pointer collides with a field
 
         var = "ABCDEFGH"
         for num in range(1, 9):
             for char in var:
-                a = pygame.Rect(130 + (var.index(char)) * 50, 100 + num * 50 , 50, 50)
+                a = pygame.Rect(130 + (var.index(char)) * 50, 100 + num * 50, 50, 50)
                 self.squares[f"{num}{char}"] = a
-
-
-
-                #checks if field is even or odd | even = white | odd = black
-
-                if num%2:
-                    pygame.draw.rect(self.screen, color_rect, a)
-
-                else:
-                    pygame.draw.rect(self.screen, color_rect_2, a)
 
         pass
         print(self.squares)
 
     def display_field(self):
+
+        img = self.font.render("PRESS ESC TO PAUSE", True, self.text_col)
+        self.screen.blit(img, (500, 750))
+
         # Display the playing field including the text
+        for i in range (1,9):
+            if self.color:
+                text = 'ABCDEFGH'
+
+                img = self.font.render(text, True, self.text_col)
+                self.screen.blit(img, (i * 50 + 150, 50))
+            else:
+                text_r = 'HGFEDCBA'
+
+                img = self.font.render(text_r, True, self.text_col)
+                x = (i * (-50)) + 900
+                self.screen.blit(img, (x, 50))
+        color_rect = 'white'
+        color_rect_2 = 'black'
+
+        # checks if field is even or odd | even = white | odd = black
+
+        for num in range(1, 8 + 1):
+            if num % 2:
+                pygame.draw.rect(self.screen, color_rect, self.squares)
+
+            else:
+                pygame.draw.rect(self.screen, color_rect_2, self.squares)
+
         pass
 
     def show_board(self, board: chess.Board):
         # Display the board onto the field
         # Also mark if it's the player turn
+
+
+
         pass
 
-    def highlight(self, field: str, color: tuple[int, int, int] = (255, 165, 0)):
+    def highlight(self, field: str, color: tuple = (255, 165, 0)):
         # Highlight a field with an orange border
         pass
 
-    def arrow(self, start: str, end: str, color: tuple[int, int, int] = (255, 165, 0)):
+    def arrow(self, start: str, end: str, color: tuple = (255, 165, 0)):
         # Draw a line from one field to another
         pass
