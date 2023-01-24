7c9948c18aa399b700990ab2e19045f3a8b9b714
Delano1q <lpezzei@tsn.at>
2023-01-24 21:55:48 +0100

    WP: not finished

--- chess4b/ui/game.py
+++ chess4b/ui/game.py
@@ -1,5 +1,6 @@
 import chess
 import pygame
+import chess4b.ui.gui_buttons as gui_buttons
 
 
 class GameDisplay:
@@ -12,37 +13,49 @@ class GameDisplay:
         self.turquoise = (52, 78, 91)
 
         self.screen: pygame.Surface = screen
-        self.squares: dict = {}
+        self.squares: dict={}
 
-        self.setup_squares()
         self.display_field()
+        self.setup_squares()
 
     def setup_squares(self):
+
+
+
+
+
+
         # Create rects to be able to check if the mouse pointer collides with a field
-        color_rect = 'white'
-        color_rect_2 = 'black'
+        color_rect = (248, 239, 221)
+        color_rect_2 = (95, 59, 47)
         var = "ABCDEFGH"
         for num in range(1, 9):
             for char in var:
-                a = pygame.Rect(130 + (var.index(char)) * 50, 100 + num * 50, 50, 50)
+                a = pygame.Rect(200 + (var.index(char)) * 75, 100 + num * 75, 75, 75)
                 if (num + (var.index(char))) % 2:
 
                     self.squares[f"{num}{char}"] = [a, color_rect]
 
                 else:
                     self.squares[f"{num}{char}"] = [a, color_rect_2]
+
+
+        pass
         print(self.squares)
 
     def display_field(self):
+
         # colors
         turquoise = (52, 78, 91)
-        white = (255, 255, 255)
 
         X = 1200
         Y = 800
         self.screen = pygame.display.set_mode([X, Y])
         pygame.display.set_caption('Game Window')
         self.screen.fill(turquoise)
+
+
+        pygame.draw.rect(self.screen, 'black', pygame.Rect(196, 171, 608, 608),4)
 
         img = self.font.render("PRESS ESC TO PAUSE", True, self.text_col)
         self.screen.blit(img, (500, 750))
@@ -64,7 +77,7 @@ class GameDisplay:
         # checks if field is even or odd | even = white | odd = black
 
         for square in self.squares:
-            pass
+
             pygame.draw.rect(self.screen, self.squares.get(square)[1], self.squares.get(square)[0])
 
         pass
@@ -73,6 +86,36 @@ class GameDisplay:
         # Display the board onto the field
         # Also mark if it's the player turn
         self.display_field()
+        black_pawn = pygame.image.load("chess4b/ui/images/black_pawn.png").convert_alpha()
+        black_queen = pygame.image.load("chess4b/ui/images/black_queen.png").convert_alpha()
+        black_king = pygame.image.load("chess4b/ui/images/black_king.png").convert_alpha()
+        black_bishop = pygame.image.load("chess4b/ui/images/black_bishop.png").convert_alpha()
+        black_knight = pygame.image.load("chess4b/ui/images/black_knight.png").convert_alpha()
+        black_rook = pygame.image.load("chess4b/ui/images/black_rook.png").convert_alpha()
+
+        white_pawn = pygame.image.load("chess4b/ui/images/white_pawn.png").convert_alpha()
+        white_queen = pygame.image.load("chess4b/ui/images/white_queen.png").convert_alpha()
+        white_king = pygame.image.load("chess4b/ui/images/white_king.png").convert_alpha()
+        white_bishop = pygame.image.load("chess4b/ui/images/white_bishop.png").convert_alpha()
+        white_knight = pygame.image.load("chess4b/ui/images/white_knight.png").convert_alpha()
+        white_rook = pygame.image.load("chess4b/ui/images/white_rook.png").convert_alpha()
+
+        black_pawn_button = gui_buttons.Button(500, 200, black_pawn, 1)
+        black_queen_button = gui_buttons.Button(125, 200, black_queen, 1)
+        black_king_button = gui_buttons.Button(157, 400, black_king, 1)
+        black_bishop_button = gui_buttons.Button(175, 500, black_bishop, 1)
+        black_knight_button = gui_buttons.Button(157, 400, black_knight, 1)
+        black_rook_button = gui_buttons.Button(157, 400, black_rook, 1)
+
+        white_pawn_button = gui_buttons.Button(500, 200, white_pawn, 1)
+        white_queen_button = gui_buttons.Button(125, 200, white_queen, 1)
+        white_king_button = gui_buttons.Button(157, 400, white_king, 1)
+        white_bishop_button = gui_buttons.Button(175, 500, white_bishop, 1)
+        white_knight_button = gui_buttons.Button(157, 400, white_knight, 1)
+        white_rook_button = gui_buttons.Button(157, 400, white_rook, 1)
+
+
+
         pass
 
     def highlight(self, field: str, color: tuple = (255, 165, 0)):
