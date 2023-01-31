8c135c4c7ba6f17439cc500f021d08700920e6b4
Thomas1q <thschmid@tsn.at>
2023-01-31 13:57:42 +0100

    Kommentare

--- chess4b/network/client.py
+++ chess4b/network/client.py
@@ -2,25 +2,25 @@ import asyncio
 import socket
 
 
-class Client:
+class Client:       #Class mit dem Namen CLient
     def __init__(self):
-        self.client_conn = None
+        self.client_conn = None     #Setzt alle Variablen auf None oder eine bestimmt Address
         self.address = ("127.0.0.1", 50000)
         self.server = None
 
     def client_connect(self):
-        self.client_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Erzeugt einen Socket
         try:
-            self.client_conn.connect(self.address)
+            self.client_conn.connect(self.address)          #Versucht eine Verbindung mkit dem Host herzustellen
             return True
         except ConnectionRefusedError:
-            print("HOST HAS TO BE SELECTED FIRST")
-            return False
+            print("HOST HAS TO BE SELECTED FIRST")          #Wenn Verbindung nicht möglich wird dass hier ausgeführt
+            return False                                    #Verhindert Crash des Programms
 
-    def write(self, message: bytes):
+    def write(self, message: bytes):                        #Write Function um mit dem Server zu Kommunizieren zu können
         self.client_conn.send(message)
 
-    def recv(self):
+    def recv(self):                                         #Read Function um Daten von dem Server zu bekommen
         return self.client_conn.recv(1024)
 
 
